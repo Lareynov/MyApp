ab559478b0ca202d76b9c8f0e0bef5b3a5256bf1
Maria <ffd@sd>
2019-11-26 10:23:21 +0300

    Initial Commit

---
+++ MyApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by Student on 26.11.2019.
+//  Copyright © 2019 Pogromist LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApp
+//
+//  Created by Student on 26.11.2019.
+//  Copyright © 2019 Pogromist LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
